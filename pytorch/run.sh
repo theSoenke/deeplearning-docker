c2bb46d4b84b5ea61b1ac8985038bbfac79ffb01
theSoenke <soenke.behrendt@gmail.com>
2019-02-28 13:22:59 +0100

    Nvidia lib workaround

--- pytorch/run.sh
+++ pytorch/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source activate pytorch
+ldconfig # workaround for https://github.com/NVIDIA/nvidia-docker/issues/854
 jupyter lab --ip 0.0.0.0 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.disable_check_xsrf=True
